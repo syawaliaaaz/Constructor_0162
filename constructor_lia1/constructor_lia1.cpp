bcc60b39e0d086f713c9a7f4294d3329a3c5586f
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-14 10:18:45 +0700

    Membuat class dari mahasiswa

---
+++ constructor_lia1/constructor_lia1.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public;
+static int nim;
+int id;
+string nama;
+
+void setID();
+void printAll;
+
+mahasiswa(string pnama) :nama(pnama) {
+	setID();
+}
+}; 
+
